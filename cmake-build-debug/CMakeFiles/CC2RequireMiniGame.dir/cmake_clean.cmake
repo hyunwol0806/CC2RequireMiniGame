b60519f6791467dd5ad8e8940031ad79514bae70
hyunwol0806 <hyunwol0806@gmail.com>
2021-05-15 13:15:22 +0900

    testCommit

---
+++ cmake-build-debug/CMakeFiles/CC2RequireMiniGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CC2RequireMiniGame.exe"
+  "CC2RequireMiniGame.exe.manifest"
+  "CC2RequireMiniGame.pdb"
+  "CMakeFiles/CC2RequireMiniGame.dir/TestHello.c.obj"
+  "libCC2RequireMiniGame.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CC2RequireMiniGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
